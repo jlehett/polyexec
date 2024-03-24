5fc95fe9616082f300c1a85cc0299a7131890daa
John Lehett <johntannerlehett@gmail.com>
2024-03-23 17:28:29 -0700

    Commit leftover.

--- ui/src/components/logs/Logs.jsx
+++ ui/src/components/logs/Logs.jsx
@@ -1,5 +1,10 @@
+import { useOnEmit } from '@psionic/emit-react';
+import LogStore from '@services/log-store/LogStore';
 
 function Logs() {
+    useOnEmit(LogStore.EVENTS.LOG_ADDED, (log) => {
+
+    });
 
     return (
         <div>
